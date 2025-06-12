a1aff47fdd8ec4b0dcf21f09251ebd1f3f018302
thirdsalinga <48078466+thirdsalinga@users.noreply.github.com>
2025-06-12 11:16:05 -0400

    Update zillowService.ts

--- common/react-zillow/src/services/zillowService.ts
+++ common/react-zillow/src/services/zillowService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = (import.meta.env.VITE_API_BASE_URL ? import.meta.env.VITE_API_BASE_URL + '/api/zillow' : '/api/zillow');
+const API_BASE_URL = 'https://cyclsales.redtechitsolutions.com/api/zillow';
 
 export interface ZillowProperty {
   id: number;
